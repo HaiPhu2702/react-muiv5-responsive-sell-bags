733ab0ee5fdd6052c5ecc90e406948b1e672fa13
HaiPhu2702 <102145150+HaiPhu2702@users.noreply.github.com>
2023-02-08 01:10:16 +0700

    2/8/2023

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {useEffect} from "react";
+import {Box, Container, Typography} from "@mui/material";
+import AppBar from "./components/apppbar";
+import Banner from "./components/banner";
+import Promotions from "./components/promotions";
+import Products from "./components/products";
+
+function App() {
+    useEffect(()=>{
+        document.title ="React-responsive"
+    },[])
+
+  return (
+    <Container
+        sx={{
+            maxWidth:'xl',
+            background:"#fff"
+        }}
+    >
+        <AppBar/>
+        <Banner />
+        <Promotions/>
+        <Box display='flex' justifyContent='center' sx={{pt:4,pb:4}}>
+           <Typography variant='h4' >Our Products</Typography>
+        </Box>
+        <Products/>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/products/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Container, Grid, useMediaQuery, useTheme} from "@mui/material";
+import {products} from "../../data";
+import SingleProductMobile from "./SingleProductMobile";
+import SingleProductDesktop from "./SingleProductDesktop";
+
+
+const Products = () => {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('md'));
+
+    const renderProduct = products.map(product => (
+        <Grid
+            item
+            key={product.id}
+            xs={2}
+            sm={4}
+            md={4}
+            display={'flex'}
+            flexDirection="column"
+                 alignItems={'center'}
+        >
+            {matches ?
+                <SingleProductMobile product={product} matches={matches}/>
+                :<SingleProductDesktop product={product} matches={matches}/>
+            }
+
+        </Grid>
+    ))
+
+    return (
+        <Container>
+            <Grid
+                container
+                spacing={{xs: 2,md: 3}}
+                justifyContent={'center'}
+                sx={{margin: "20px 4px 10px 4px"}}
+                columns={{xs:4,sm:8,md:12}}
+            >
+                {renderProduct}
+            </Grid>
+        </Container>
+    );
+};
+
+export default Products;
